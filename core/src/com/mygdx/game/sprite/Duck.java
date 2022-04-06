1b1196434fcf317a564df437e6729f40017e8e8a
AlliGaToR165 <semen.chagin@mail.ru>
2022-04-06 21:38:43 +0500

    Add Project

---
+++ core/src/com/mygdx/game/sprite/Duck.java
@@ -0,0 +1,84 @@
+package com.mygdx.game.sprite;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class Duck {
+    private Texture texture;
+    private Vector2 position;
+    private Vector2 velocity;
+    private Rectangle bounds;
+    private boolean isKilled;
+
+    private float time;
+    private float angle = 0;
+    private final int WIDTH = 252;
+    private final int HEIGHT = 256;
+
+    public Duck(Vector2 position, Vector2 velocity) {
+        this.position = position;
+        this.velocity = velocity;
+        this.texture = new Texture("duck.png");
+        bounds = new Rectangle(position.x,position.y, WIDTH,HEIGHT);
+    }
+
+    public void render(SpriteBatch batch) {
+        int frame = (int) (time / 0.1f);
+        frame = frame % 4;
+        batch.draw(texture, position.x, position.y, frame * WIDTH, 0, WIDTH, HEIGHT);
+        batch.draw(texture, position.x, position.y, WIDTH/2, HEIGHT/2, WIDTH, HEIGHT,1,1,angle,frame * WIDTH,0, WIDTH,HEIGHT,false,false);
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+
+    public void update(float delta) {
+        time+= delta;
+        position.add(velocity);
+        velocity.x += 0.05f * delta;
+        bounds.setPosition(position.x,position.y);
+        if (isKilled) {
+            angle+=10;
+            velocity.y--;
+        }
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
+
+    public Vector2 getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(Vector2 velocity) {
+        this.velocity = velocity;
+    }
+
+    public Texture getTexture() {
+        return texture;
+    }
+
+    public void setTexture(Texture texture) {
+        this.texture = texture;
+    }
+
+    public boolean isKilled() {
+        return isKilled;
+    }
+
+    public void setKilled(boolean killed) {
+        isKilled = killed;
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+}
---
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -0,0 +1,134 @@
+package com.mygdx.game.states;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.InputHandler;
+import com.mygdx.game.Score;
+import com.mygdx.game.sprite.Duck;
+
+public class PlayState extends AbstractState{
+    private BitmapFont font;
+    private Texture sight;
+    private Texture background1, background2,background3,background4;
+    float xMax, xCoordBg1, xCoordBg2,xCoordBg3,xCoordBg4;
+    private Texture gras;
+    private Texture ground;
+    private int score;
+    private final int DUCKS_COUNT = 3;
+    private final int UFOS_COUNT = 3;
+    private Duck[] ducks;
+    final int BACKGROUND_MOVE_SPEED = 50;
+    private int v;
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+        background1 = new Texture("sky.jpg");
+        background2 = new Texture("sky.jpg");
+        background3 = new Texture("sky.jpg");
+        background4 = new Texture("sky.jpg");
+        sight = new Texture("sight.png");
+        font = new BitmapFont(Gdx.files.internal("myfont.fnt"),Gdx.files.internal("myfont.png"),false);
+        gras = new Texture("gras.png");
+        ground = new Texture("ground.png");
+        score = 0;
+        ducks = new Duck[DUCKS_COUNT];
+        ducks[0] = new Duck(new Vector2(0,0),new Vector2(1.5f,1.0f));
+        for (int i = 1; i < DUCKS_COUNT; i++) {
+            ducks[i] = new Duck(new Vector2(ducks[i - 1].getPosition().x + 100, ducks[i - 1].getPosition().y - MathUtils.random(200,500)),
+                    new Vector2(ducks[i - 1].getVelocity().x,ducks[i - 1].getVelocity().y));
+        }
+
+        xMax = background1.getWidth();
+        xCoordBg1 = 0;
+        xCoordBg2=xCoordBg1+xMax;
+        xCoordBg3=xCoordBg2+xMax;
+        xCoordBg4=xCoordBg3+xMax;
+        v=1;
+    }
+
+    @Override
+    public void update(float delta) {
+        xCoordBg1 -= BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+        xCoordBg2 -= BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+        xCoordBg3 -= BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+        xCoordBg4 -= BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+
+        for (int i = 0; i < DUCKS_COUNT; i++) {
+            ducks[i].update(delta);
+        }
+        switch (v) {
+            case 2:
+                if (xCoordBg2 + xMax <= 0) {
+                    xCoordBg2 = xCoordBg1 + xMax;
+                    v = 3;
+                }
+            case 3:
+                if (xCoordBg3 + xMax <= 0) {
+                    xCoordBg3 = xCoordBg2 + xMax;
+                    v = 4;
+                }
+            case 4:
+                if (xCoordBg4 + xMax <= 0) {
+                    xCoordBg4 = xCoordBg3 + xMax;
+                    v = 1;
+                }
+            case 1:
+                if (xCoordBg1 + xMax <= 0) {
+                    xCoordBg1 = xCoordBg4 + xMax;
+                    v = 2;
+                }
+        }
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+        batch.begin();
+        batch.draw(background1,xCoordBg1,-50);
+        batch.draw(background2,xCoordBg2,-50);
+        batch.draw(background3,xCoordBg3,-50);
+        batch.draw(background4,xCoordBg4,-50);
+
+        for (int i = 0; i < DUCKS_COUNT; i++) {
+            ducks[i].render(batch);
+            if (InputHandler.isClicked()) {
+                if (ducks[i].getBounds().contains(InputHandler.getMousePosition().x,InputHandler.getMousePosition().y)) {
+                    ducks[i].setKilled(true);
+                    score++;
+                    Score.setScores(score);
+                }
+            }
+        }
+        font.draw(batch, "Scores: " + score, 10,Gdx.graphics.getHeight() - 20);
+        int x= 0;
+        do {
+            batch.draw(ground,x,0);
+            //batch.draw(gras,-30 + x,-80);
+            x+=ground.getWidth();
+        } while (x <= Gdx.graphics.getWidth());
+        batch.draw(sight, InputHandler.getMousePosition().x - sight.getWidth()/2,InputHandler.getMousePosition().y - sight.getHeight()/2);
+        if ((ducks[DUCKS_COUNT - 1].getBounds().x -ducks[DUCKS_COUNT - 1].getBounds().width> Gdx.graphics.getWidth()
+                || ducks[DUCKS_COUNT - 1].isKilled())){
+            gsm.set(new GameOverState(gsm));
+        }
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        sight.dispose();
+        for (int i = 0; i < DUCKS_COUNT; i++) {
+            ducks[i].dispose();
+        }
+        font.dispose();
+        //gras.dispose();
+        background1.dispose();
+        background2.dispose();
+        background3.dispose();
+        background4.dispose();
+        ground.dispose();
+    }
+}
